2146cc6c82fdb1cfd0d97079ad788ab975fb5efd
Bydmak <bydmak.study@gmail.com>
2018-04-26 20:32:41 +0300

    New(Denis)

--- src/index.js
+++ src/index.js
@@ -1,26 +1,41 @@
 import jsonQuery from 'json-query';
 import nestedProperty from 'nested-property';
+import jsonq from 'jsonq';
 import data from './data.json';
 
 function GetForgeIdByCoordinates(sector, row, seat) {
     console.log(`Getting forgeId by coordinates (Sector ${sector}, Row ${row}, Seat ${seat}). Result: `);
-    var answer = 0;
-    //var answer = nestedProperty.get(data. ???);
+    var answer = nestedProperty.get(data,`sectors.${sector-1}.rows.${row-1}.seats.${seat-1}.forgeId.${1}`);
 
     console.log(answer);
     return answer;
 }
 
-function GetCoordinatesByForgeId(forgeId) {
-    console.log(`Getting coordinates by forgeId (ForgeId: ${forgeId}). Result: `);
-    var sector = 0;
-    var row = 0;
-    var seat = 0;
+function GetCoordinatesByForgeId(argumentId) {
+    console.log(`Getting coordinates by forgeId (forgeId: ${argumentId}). Result: `);
+    var jsonQdata = jsonQ(data);
+    var id = jsonQdata.find('forgeId', function() {
+        if (this instanceof Array)
+            return this[1] === argumentId;
+    });
+    var path = id.path();
+    var sector = parseInt(path[1]) + 1;
+    var row = parseInt(path[3]) + 1;
+    var seat = parseInt(path[5]) + 1;
+
     var answer = [sector, row, seat]
-    //var answer = jsonQuery(???);
-    
     console.log(answer);
     return answer;
+}
+
+function GetNumberRowsBySector(sector) {
+    console.log(`Getting all rows by sector (Sector ${sector}). Result: `);
+    var answer = nestedProperty.get(data,`sectors.${sector-1}.rows`).length;
+    console.log(answer);
+    return answer;
+}
+function GetNumberOfSeats(sector,row) {
+    console.log(`Getting all rows by sector (Sector ${sector}). Result: `);
 }
 
 GetForgeIdByCoordinates(6, 2, 9);
@@ -29,4 +44,8 @@ GetForgeIdByCoordinates(6, 5, 10);
 
 GetCoordinatesByForgeId(6358);
 GetCoordinatesByForgeId(4634);
-GetCoordinatesByForgeId(4797);
+GetCoordinatesByForgeId(4842);
+
+GetNumberRowsBySector(1);
+GetNumberRowsBySector(6);
+GetNumberRowsBySector(2);
